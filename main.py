70c47aeb3473ab3de90a32741522e0a9abbfbceb
micmarc <dev@michaelmarcus.net>
2023-03-14 22:03:38 -0400

    Remove sys import, use exit instead

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-
 from downloader import download
 
 if __name__ == '__main__':
@@ -7,12 +5,10 @@ if __name__ == '__main__':
         year = int(input('Enter year to download: '))
     except ValueError:
         print('ERROR: Invalid year!')
-        sys.exit(1)
+        exit(1)
 
     success = download(puzzle_type='acrostic', pub_year=year) and download(puzzle_type='variety', pub_year=year)
     if success:
         print('All done!')
     else:
         print('There was an issue downloading puzzles.')
-
-    sys.exit(0 if success else 1)

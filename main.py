55e97d07e2530aeb4943fdab56f93737e74fa2b0
micmarc <dev@michaelmarcus.net>
2023-03-14 22:16:47 -0400

    Remove exit call; fix windows path separator

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from downloader import download
 
 if __name__ == '__main__':
-    try:
-        year = int(input('Enter year to download: '))
-    except ValueError:
-        print('ERROR: Invalid year!')
-        exit(1)
+    # try:
+    year = int(input('Enter year to download: '))
+    # except ValueError:
+    #     print('ERROR: Invalid year!')
+    #     exit(1)
 
     success = download(puzzle_type='acrostic', pub_year=year) and download(puzzle_type='variety', pub_year=year)
     if success:
